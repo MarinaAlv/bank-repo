80a04fb655834bae2cc169b63ea8ec09d4b3b924
Marina <marvarado19@gmail.com>
2024-09-30 21:26:43 -0700

    workingInAppJsx

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,12 @@
-import { configureStore } from "@reduxjs/toolkit";
+import {configureStore} from '@reduxjs/toolkit';
+import {transfer} from '../features/transactions/transactionsSlice';
+import Transactions from '../features/transactions/Transactions';
 
 // TODO: Configure the store to use the reducer from the transactions slice.
-const store = configureStore();
+const store = configureStore({
+  reducer: {
+    transactions: transactionsReducer,
+  },
+});
 
 export default store;
